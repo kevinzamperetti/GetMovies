2f26167ebc49f9836098c4a50c72f84fccf358fd
Kevin Zamperetti Schepke <kevin.zamperetti92@gmail.com>
2019-11-12 00:45:52 -0300

    Ajustado layout das telas de listagem, centralizando filmes e ajustado cores de fonte do detalhe.

--- app/src/main/java/com/br/getmovies/ListTopRatedMoviesActivity.java
+++ app/src/main/java/com/br/getmovies/ListTopRatedMoviesActivity.java
@@ -2,17 +2,14 @@ package com.br.getmovies;
 
 import android.content.Context;
 import android.net.ConnectivityManager;
+import android.os.Bundle;
 import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.DisplayMetrics;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.ProgressBar;
 import android.widget.TextView;
